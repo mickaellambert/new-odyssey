c7dd2e74de732876086fd9641bd8db5f987d6e14
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2023-10-30 14:53:36 +0100

    Delete old route and add css variables

--- src/routes.php
+++ src/routes.php
@@ -7,5 +7,5 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
+    '' => ['HomeController', 'index',], 
 ];
